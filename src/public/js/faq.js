544215f00519aa111b99120b896d98c636ffd41c
keyur diwan <keyurdiwan009@gmail.com>
2021-12-17 09:26:30 +0530

    add js files

---
+++ src/public/js/faq.js
@@ -0,0 +1,694 @@
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+var sol = document.getElementById("abc2");
+
+sol.addEventListener("click", solution);
+
+function solution() {
+  var btn991 = document.getElementById("top2");
+
+  btn991.style.display = "none";
+  var btn99189 = document.getElementById("abc2");
+  btn99189.style.borderTop = "3px solid green";
+  var btn991892 = document.getElementById("abc1");
+  btn991892.style.borderTop = "1px solid transparent";
+}
+var btn998 = document.getElementById("abc1");
+btn998.addEventListener("click", home123);
+function home123() {
+  var btn9913 = document.getElementById("top2");
+  btn9913.style.display = "block";
+
+  var btn9918990 = document.getElementById("abc1");
+  btn9918990.style.borderTop = "3px solid green";
+  var btn9918929 = document.getElementById("abc2");
+  btn9918929.style.borderTop = "1px solid transparent";
+}
+
+var btn99 = document.getElementById("login-btn2");
+btn99.addEventListener("click", hush);
+function hush() {
+  window.location.href = "/home/signup";
+}
+
+var btn1 = document.getElementById("login-btn");
+btn1.addEventListener("click", hhhh);
+function hhhh() {
+  window.location.href = "/home/login";
+}
+
+var q1 = document.getElementById("q1");
+q1.addEventListener("click", test);
+
+function test() {
+  var link = q1.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q2 = document.getElementById("q2");
+q2.addEventListener("click", test2);
+
+function test2() {
+  var link = q2.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q3 = document.getElementById("q3");
+q3.addEventListener("click", test3);
+
+function test3() {
+  var link = q3.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q4 = document.getElementById("q4");
+q4.addEventListener("click", test4);
+
+function test4() {
+  var link = q4.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q5 = document.getElementById("q5");
+q5.addEventListener("click", test5);
+
+function test5() {
+  var link = q5.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q6 = document.getElementById("q6");
+q6.addEventListener("click", test6);
+function test6() {
+  var link = q6.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q7 = document.getElementById("q7");
+q7.addEventListener("click", test7);
+function test7() {
+  var link = q7.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q8 = document.getElementById("q8");
+q8.addEventListener("click", test8);
+function test8() {
+  var link = q8.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q9 = document.getElementById("q9");
+q9.addEventListener("click", test9);
+function test9() {
+  var link = q9.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q10 = document.getElementById("q10");
+q10.addEventListener("click", test10);
+function test10() {
+  var link = q10.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q11 = document.getElementById("q11");
+q11.addEventListener("click", test11);
+function test11() {
+  var link = q11.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q12 = document.getElementById("q12");
+q12.addEventListener("click", test12);
+function test12() {
+  var link = q12.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q13 = document.getElementById("q13");
+q13.addEventListener("click", test13);
+function test13() {
+  var link = q13.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q14 = document.getElementById("q14");
+q14.addEventListener("click", test14);
+function test14() {
+  var link = q14.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q15 = document.getElementById("q15");
+q15.addEventListener("click", test15);
+function test15() {
+  var link = q15.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q16 = document.getElementById("q16");
+q16.addEventListener("click", test16);
+function test16() {
+  var link = q16.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q17 = document.getElementById("q17");
+q17.addEventListener("click", test17);
+function test17() {
+  var link = q17.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q18 = document.getElementById("q18");
+q18.addEventListener("click", test18);
+function test18() {
+  var link = q18.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q19 = document.getElementById("q19");
+q19.addEventListener("click", test19);
+function test19() {
+  var link = q19.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q20 = document.getElementById("q20");
+q20.addEventListener("click", test20);
+function test20() {
+  var link = q20.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q21 = document.getElementById("q21");
+q21.addEventListener("click", test21);
+function test21() {
+  var link = q21.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q22 = document.getElementById("q22");
+q22.addEventListener("click", test22);
+function test22() {
+  var link = q22.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q23 = document.getElementById("q23");
+q23.addEventListener("click", test23);
+function test23() {
+  var link = q23.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q24 = document.getElementById("q24");
+q24.addEventListener("click", test24);
+function test24() {
+  var link = q24.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q25 = document.getElementById("q25");
+q25.addEventListener("click", test25);
+function test25() {
+  var link = q25.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q26 = document.getElementById("q26");
+q26.addEventListener("click", test26);
+function test26() {
+  var link = q26.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q27 = document.getElementById("q27");
+q27.addEventListener("click", test27);
+function test27() {
+  var link = q27.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q28 = document.getElementById("q28");
+q28.addEventListener("click", test28);
+function test28() {
+  var link = q28.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q29 = document.getElementById("q29");
+q29.addEventListener("click", test29);
+function test29() {
+  var link = q29.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q30 = document.getElementById("q30");
+q30.addEventListener("click", test30);
+function test30() {
+  var link = q30.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q31 = document.getElementById("q31");
+q31.addEventListener("click", test31);
+function test31() {
+  var link = q31.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q32 = document.getElementById("q32");
+q32.addEventListener("click", test32);
+function test32() {
+  var link = q32.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q33 = document.getElementById("q33");
+q33.addEventListener("click", test33);
+function test33() {
+  var link = q33.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q34 = document.getElementById("q34");
+q34.addEventListener("click", test34);
+function test34() {
+  var link = q34.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q35 = document.getElementById("q35");
+q35.addEventListener("click", test35);
+function test35() {
+  var link = q35.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q36 = document.getElementById("q36");
+q36.addEventListener("click", test36);
+function test36() {
+  var link = q36.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q37 = document.getElementById("q37");
+q37.addEventListener("click", test37);
+function test37() {
+  var link = q37.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q38 = document.getElementById("q38");
+q38.addEventListener("click", test38);
+function test38() {
+  var link = q38.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q39 = document.getElementById("q39");
+q39.addEventListener("click", test39);
+function test39() {
+  var link = q39.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q40 = document.getElementById("q40");
+q40.addEventListener("click", test40);
+function test40() {
+  var link = q40.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q41 = document.getElementById("q41");
+q41.addEventListener("click", test41);
+function test41() {
+  var link = q41.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+var q42 = document.getElementById("q42");
+q42.addEventListener("click", test42);
+function test42() {
+  var link = q42.innerHTML;
+
+  let arr;
+  arr = localStorage.getItem("data");
+  if (arr == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(localStorage.getItem("data"));
+  }
+  arr.push(link);
+  localStorage.setItem("data", JSON.stringify(arr));
+}
+
+var q4892 = document.getElementById("power-off132254");
+q4892.addEventListener("click", test4277);
+
+function test4277() {
+  window.location.href = "/home/faq1";
+}
